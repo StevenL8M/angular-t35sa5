67618bc764d72f3b75950c8cc2576be9b06b1928
StevenL8M <steven.le_moine@outlook.com>
2020-02-13 12:06:53 +0100

    1 file modified, 1 added, 1 deleted

---
+++ src/app/@shared/user-service.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { User } from '../models/user/user.component'
+
+@Injectable()
+export class UserServiceService {
+  user : User;
+  constructor() { }
+
+  onSubmit(){
+    
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { AppComponent } from './app.component';
 import { LoginformComponent } from './loginform/loginform.component';
 import { AuthService } from './@shared/auth.service';
 import { User } from './models/user/user.component';
+import { UserServiceService } from './@shared/user-service.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, NgbModule ],
   declarations: [ AppComponent, LoginformComponent, User ],
   bootstrap:    [ AppComponent ],
-  providers: [AuthService]
+  providers: [AuthService, UserServiceService]
 })
 export class AppModule { }
