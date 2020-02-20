a2e4f7ccc951e1b1e7dda570eec1e6d0e480f2d1
StevenL8M <steven.le_moine@outlook.com>
2020-02-20 12:23:02 +0100

    1 file modified

--- src/app/@shared/idea.service.ts
+++ src/app/@shared/idea.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Idea } from '../models/idea'
 
 @Injectable()
 export class IdeaService {
-  
+  ideas : Array<Idea>;
 
   constructor() { }
+
+  getIdeas(){
+    return this.ideas;
+  }
+
+  addIdea(contenu:string, auteur:string){
+    this.ideas.push({ auteur: auteur, contenu:contenu, voteUp:0, voteDown:0 });
+  }
+
+  getSpecificIdea(index:number){
+    return this.ideas.
+  }
 
 }
