59be37b9da39f8884e8c1de747d7ab737d8e65a1
StevenL8M <steven.le_moine@outlook.com>
2020-02-25 12:23:36 +0100

    1 file modified

--- src/app/liste-ideas/liste-ideas.component.ts
+++ src/app/liste-ideas/liste-ideas.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserServiceService } from '../@shared/user-service.service';
 import { IdeaService } from '../@shared/idea.service';
 import { Idea } from '../models/idea';
+import { User } from '../models/user';
 
 @Component({
   selector: 'liste-ideas',
@@ -10,10 +11,13 @@ import { Idea } from '../models/idea';
 })
 export class ListeIdeasComponent implements OnInit {
   public idees:Idea[];
-  constructor(private ideaService:IdeaService) {}
+  private user:User;
+  constructor(private ideaService:IdeaService,
+              private userService:UserServiceService) {}
 
   ngOnInit() {
     this.idees = this.ideaService.getIdeas();
+    this.user = this.userService.getUser();
   }
 
   down(idee:Idea){
@@ -22,6 +26,13 @@ export class ListeIdeasComponent implements OnInit {
 
   up(idee:Idea){
     idee.voteUp++;
+  }
+
+  delete(idee:Idea){
+    if(this.user.pseudo == idee.auteur){
+      let index:number=this.ideaService.getIdeas().indexOf(idee);
+        this.ideaService.ideas.splice(index, 1);
+    }
   }
 
 }
