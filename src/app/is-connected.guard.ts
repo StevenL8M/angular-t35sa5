fe23c32257cba68ac7f0dbd9a358c0189f98522e
StevenL8M <steven.le_moine@outlook.com>
2020-02-28 10:49:05 +0100

    1 file added

---
+++ src/app/is-connected.guard.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserServiceService } from '@shared/user-service.service';
+import { User } from 'models/user';
+
+@Injectable()
+export class IsConnectedGuard implements CanActivate {
+  
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return true;
+  }
+}
